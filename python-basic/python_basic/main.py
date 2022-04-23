7c0f5efd0734f65f16fd105ab273e6b74db41749
renmurakami <autoeyy@gmail.com>
2022-04-23 11:38:23 +0900

    func

--- python-basic/python_basic/main.py
+++ python-basic/python_basic/main.py
@@ -1,3 +1,12 @@
 helloWorld = "hello world from python"
 
 print(helloWorld)
+
+def easyMath(x, y):
+    doubleX = x * 2
+    ans = doubleX + y
+    return ans
+
+answer = easyMath(5,9)
+
+print(answer)
